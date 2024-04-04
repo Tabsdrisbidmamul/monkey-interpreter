4b7a4271fd4c0f861d19618c5afbec5b35438bbf
Idris Khan <idris.khan303@gmail.comm>
2024-04-04 13:56:56 +0100

    Added tokens for interpreter

---
+++ src/token/token.go
@@ -0,0 +1,35 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// Identifiers + literalls
+	IDENT = "IDENT" // add, foobar, x, y - variables
+	INT = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS = "+"
+
+	// Delimiters
+	COMMA = ","
+	SEMICOLON = ";"
+
+	// Special Characters
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "{"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
