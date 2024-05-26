6a9f1637d86c126cb106549f135d2aebf496d1d5
Idris Khan <idris.khan303@gmail.comm>
2024-05-26 16:34:56 +0100

    Update return test case to check call expression is working as intended

--- src/evaluator/evaluator_test.go
+++ src/evaluator/evaluator_test.go
@@ -139,6 +139,25 @@ func TestReturnStatements(t *testing.T) {
       return 1;
     }
     `, 10},
+		{
+			`
+let f = fn(x) {
+  return x;
+  x + 10;
+};
+f(10);`,
+			10,
+		},
+		{
+			`
+let f = fn(x) {
+   let result = x + 10;
+   return result;
+   return 10;
+};
+f(10);`,
+			20,
+		},
 	}
 
 	for _, tc := range tests {
